7f6944be385660f1e5014eb7f745e9e1b0cbf52f
Arsalan Qureshi <sp16-bcs-048@student.comsats.edu.pk>
2021-12-29 04:10:20 +0500

    Code refactoring

--- src/main/kotlin/com/learning/LearningKotlin/LearningKotlinApplication.kt
+++ src/main/kotlin/com/learning/LearningKotlin/LearningKotlinApplication.kt
@@ -7,13 +7,7 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.ec2.Ec2Client
 
 @SpringBootApplication
-class LearningKotlinApplication {
-    @Bean
-    fun getEC2Client(): Ec2Client = Ec2Client
-        .builder()
-        .region(Region.US_EAST_2)
-        .build()
-}
+class LearningKotlinApplication
 
 fun main(args: Array<String>) {
     runApplication<LearningKotlinApplication>(*args)
